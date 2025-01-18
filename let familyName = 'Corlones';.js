5fb428446b75566c49cfeb8668ccf88d9877405c
letsbehonest <tglenn0903@gmail.com>
2025-01-18 06:46:20 -0800

    attempt 2

---
+++ let familyName = 'Corlones';.js
@@ -0,0 +1,4 @@
+let familyName = 'Corlones';
+
+malenameStorage = ['Todd', 'Barry', 'Hydraxus', 'Grin', 'Samson', 'Teradaxes', 'Blimy', 'Caracotta', 'Seel', 'Richard', 'Alex', 'Matt', 'Pimblett', 'Tom', 'Aspinall', 'Seratosis'];
+
